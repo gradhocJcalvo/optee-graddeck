a42cca1fba1264e88a0009e702be0da39703d185
Lionel Debieve <lionel.debieve@foss.st.com>
2021-11-06 00:00:56 +0100

    plat-stm32mp1: chip and platform identification
    New platform function to get the chip identification using
    DBGMCU SoC register.
    
    Signed-off-by: Lionel Debieve <lionel.debieve@foss.st.com>
    Change-Id: I56727a701ded07e849c17fdf06e156ee3b53d0c7

--- core/arch/arm/plat-stm32mp1/stm32_util.h
+++ core/arch/arm/plat-stm32mp1/stm32_util.h
@@ -16,6 +16,10 @@
 #include <tee_api_types.h>
 #include <types_ext.h>
 
+/* SoC versioning and device ID */
+TEE_Result stm32mp1_dbgmcu_get_chip_version(uint32_t *chip_version);
+TEE_Result stm32mp1_dbgmcu_get_chip_dev_id(uint32_t *chip_dev_id);
+
 /* Backup registers and RAM utils */
 vaddr_t stm32mp_bkpreg(unsigned int idx);
 
