e382655df7622351dcd6b175e8254e531790446a
Lionel Debieve <lionel.debieve@foss.st.com>
2021-12-06 11:43:30 +0100

    drivers: add stm32 RIFSC support
    Add the RIFSC new driver support.
    
    RIFSC (RIF Security Controller) is responsible for the isolation
    of hardware resources like memory or peripherals. It is composed of:
    
    -RISC registers(slave peripherals) with RISUP(Resource Isolation
    Slave Unit for Peripherals) OR RISAL(Resource Isolation Slave Unit
    for Address space - Lite) logics.
    -RIMC registers(Non RIF-Aware masters counterpart) with RIMU
    (Resource Isolation Master Unit) logic. It is possible for a master to
    inherit from its slave port(RISUP) configuration.
    
    This driver parses the RIFSC device tree configuration and applies
    it to put the firewall in place. Therefore, the device tree is
    mandatory.
    
    Change-Id: I2e320ad9f83aa945e8232158c982e386e78ccf32
    Signed-off-by: Gatien CHEVALLIER <gatien.chevallier@st.com>
    Signed-off-by: Ludovic Barre <ludovic.barre@foss.st.com>
    Signed-off-by: Christophe Kerello <christophe.kerello@foss.st.com>

---
+++ core/include/drivers/stm32_rifsc.h
@@ -0,0 +1,23 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright (c) 2020, STMicroelectronics
+ */
+#ifndef __DRIVERS_STM32_RIFSC_H
+#define __DRIVERS_STM32_RIFSC_H
+
+#include <types_ext.h>
+#include <util.h>
+
+struct risup_cfg {
+	uint32_t id;
+	bool sec;
+	bool priv;
+	uint32_t cid_attr;
+};
+
+struct rimu_cfg {
+	uint32_t id;
+	uint32_t attr;
+};
+
+#endif /* __DRIVERS_STM32_RIFSC_H */
