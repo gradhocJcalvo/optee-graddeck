218960efb9737c5d3d209978af3a1b3f523924d3
Gabriel Fernandez <gabriel.fernandez@foss.st.com>
2021-12-10 14:39:14 +0100

    plat-stm32mp2: add rcc helper function
    Add dedicate include file for RCC to add helper function.
    
    Change-Id: I72e967e2d899e111176c230a5b17db20fd2a7c6d
    Signed-off-by: Gabriel Fernandez <gabriel.fernandez@foss.st.com>

---
+++ core/include/drivers/stm32mp2_rcc_util.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright (C) STMicroelectronics 2023 - All Rights Reserved
+ */
+
+#ifndef __DRIVERS_STM32MP2_RCC_UTIL_H__
+#define __DRIVERS_STM32MP2_RCC_UTIL_H__
+
+/* Platform util for the RCC drivers */
+vaddr_t stm32_rcc_base(void);
+
+#endif /*__DRIVERS_STM32MP2_RCC_UTIL_H__*/
