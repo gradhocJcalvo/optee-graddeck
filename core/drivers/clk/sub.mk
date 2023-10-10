0d73fd94513c624727caa80ec24765a46971f0fb
Gabriel Fernandez <gabriel.fernandez@foss.st.com>
2023-10-10 15:27:38 +0200

    clk: stm32mp25: add support of pinctrl and clock for RCC node
    Use the generic stm32mp clock driver to support the OP-TEE initialization
    of pinctrl and clock defined in the RCC sub-node with compatible "st,rcc".
    
    Change-Id: I42f13800a6b3f9ddad06a6594e4bd8713281b1e0
    Signed-off-by: Gabriel Fernandez <gabriel.fernandez@foss.st.com>

--- core/drivers/clk/sub.mk
+++ core/drivers/clk/sub.mk
@@ -4,6 +4,6 @@ srcs-$(CFG_DRIVERS_CLK_FIXED) += fixed_clk.c
 srcs-$(CFG_STM32MP_CLK_CORE) += clk-stm32-core.c
 srcs-$(CFG_STM32MP13_CLK) += clk-stm32mp13.c clk-stm32-rcc.c
 srcs-$(CFG_STM32MP15_CLK) += clk-stm32mp15.c clk-stm32-rcc.c
-srcs-$(CFG_STM32MP25_CLK) += clk-stm32mp25.c
+srcs-$(CFG_STM32MP25_CLK) += clk-stm32mp25.c clk-stm32-rcc.c
 
 subdirs-$(CFG_DRIVERS_SAM_CLK) += sam
