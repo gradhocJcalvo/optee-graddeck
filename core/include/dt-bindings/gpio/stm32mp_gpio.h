396099fd8c0889fe155e0eb9e5cf6448171b148e
Gatien Chevallier <gatien.chevallier@foss.st.com>
2023-12-15 11:31:46 +0100

    dt-bindings: add TZPROT macro
    Add TZPROT macro to define the security level for GPIOs
    
    Signed-off-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
    Change-Id: I23b2f240f38652705a4b77c9e1827a668b05391b

---
+++ core/include/dt-bindings/gpio/stm32mp_gpio.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: (GPL-2.0-or-later OR BSD-3-Clause) */
+/*
+ * Copyright (C) STMicroelectronics 2023 - All Rights Reserved
+ */
+
+#ifndef _DT_BINDINGS_GPIO_STM32MP_GPIO_H
+#define _DT_BINDINGS_GPIO_STM32MP_GPIO_H
+
+#include <dt-bindings/gpio/gpio.h>
+
+/* Macro to define the security for GPIO */
+#define TZPROT(id) (1 << (id))
+
+#endif
