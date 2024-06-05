c4cd8e8e26683087257db0a4ccf49c2e4f0719f2
Etienne Carriere <etienne.carriere@foss.st.com>
2024-06-05 14:35:35 +0200

    core: scmi-server: fix optee-stm32mp* modules directives
    Remove conditional use of scpfw-embed-product-module macro in
    optee-stm32mp1 and optee-stm32mp2 SCP-firmware products to
    ensure the directive CFG_SCPFW_MOD_* are provided to SCP-firmware
    CMake configuration unless what SCP-firmware generate files
    fwk_module_list.c and fwk_module_idx.h are not generated with update
    config switches for these product specific modules.
    
    Change-Id: If291eecbf191042704037c394327b3114a36db58
    Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/385417
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Domain-Review: Thomas BOURGOIN <thomas.bourgoin@foss.st.com>
    Reviewed-by: Thomas BOURGOIN <thomas.bourgoin@foss.st.com>
    ACI: CIBUILD <MDG-smet-aci-builds@list.st.com>

--- core/lib/scmi-server/sub-optee-stm32mp1.mk
+++ core/lib/scmi-server/sub-optee-stm32mp1.mk
@@ -2,8 +2,6 @@ scpfw-incdirs-y += $(scpfw-path)/product/optee-stm32mp1/include
 
 srcs-y += $(scpfw-path)/product/optee-stm32mp1/fw/config_all.c
 
-ifeq ($(CFG_SCPFW_MOD_PSU_OPTEE_REGULATOR),y)
 $(eval $(call scpfw-embed-product-module,psu_optee_regulator))
-endif
 
 scpfw-cmake-flags-y += -DCFG_STM32MP13=$(CFG_STM32MP13)
--- core/lib/scmi-server/sub-optee-stm32mp2.mk
+++ core/lib/scmi-server/sub-optee-stm32mp2.mk
@@ -2,11 +2,5 @@ scpfw-incdirs-y += $(scpfw-path)/product/optee-stm32mp2/include
 
 srcs-y += $(scpfw-path)/product/optee-stm32mp2/fw/config_all.c
 
-ifeq ($(CFG_SCPFW_MOD_PSU_OPTEE_REGULATOR),y)
 $(eval $(call scpfw-embed-product-module,psu_optee_regulator))
-endif
-
-ifeq ($(CFG_SCPFW_MOD_STM32_PD),y)
 $(eval $(call scpfw-embed-product-module,stm32_pd))
-endif
-
