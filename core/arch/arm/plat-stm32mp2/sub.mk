134e6178d2905437bf98ef728bc2efd71e8ee003
Yannick Fertre <yannick.fertre@foss.st.com>
2024-06-27 15:45:44 +0200

    plat-stm32mp2: display: add wrapper
    Add a display wrapper to register all secure devices which
    need to be initialized during OPTEE init sequence.
    
    Change-Id: I6eefc320fb326878a60d82e406d50f6c23a392d5
    Signed-off-by: Yannick Fertre <yannick.fertre@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/391784
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Reviewed-by: Thomas BOURGOIN <thomas.bourgoin@foss.st.com>

--- core/arch/arm/plat-stm32mp2/sub.mk
+++ core/arch/arm/plat-stm32mp2/sub.mk
@@ -3,5 +3,7 @@ global-incdirs-y += .
 srcs-y += main.c
 srcs-y += stm32mp_pm.c
 subdirs-y += drivers
+
+srcs-$(CFG_DISPLAY) += display.c
 srcs-$(CFG_SCMI_CORTEXM_AGENT) += scmi_cortexm_agent.c
 srcs-$(CFG_SCMI_SCPFW) += scmi_server_scpfw.c
