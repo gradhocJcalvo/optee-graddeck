4ms/optee-stm32mp25

core: dt: allow using _fdt_read_paddr in drivers
This patch allows the use of _fdt_read_paddr API outside dt.c

Change-Id: I07a5494f878845a576e6534afb6b7dc97a330bfa
Signed-off-by: Christophe Kerello <christophe.kerello@foss.st.com>
Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/259958
Reviewed-by: CITOOLS <MDG-smet-aci-reviews@list.st.com>
Reviewed-by: Christophe KERELLO <christophe.kerello@st.com>
Reviewed-by: CIBUILD <MDG-smet-aci-builds@list.st.com>
Reviewed-by: Gatien CHEVALLIER <gatien.chevallier@st.com>
Reviewed-by: Lionel DEBIEVE <lionel.debieve@foss.st.com>
Tested-by: Christophe KERELLO <christophe.kerello@st.com>

core/include/kernel/dt.h
@@ -168,6 +168,11 @@ int dt_enable_secure_status(void *fdt, int node);
  */
 
 /*
+ * Return the base address from a cell or (paddr_t)-1 in case of error
+ */
+paddr_t _fdt_read_paddr(const uint32_t *cell, int n);
+
+/*
  * Return the base address for the "reg" property of the specified node or
  * (paddr_t)-1 in case of error
  */
@@ -246,6 +251,12 @@ static inline const struct dt_driver *dt_find_compatible_driver(
 
 static inline int dt_map_dev(const void *fdt __unused, int offs __unused,
 			     vaddr_t *vbase __unused, size_t *size __unused)
+{
+	return -1;
+}
+
+static inline paddr_t _fdt_read_paddr(const uint32_t *cell __unused,
+				      int n __unused)
 {
 	return -1;
 }

core/kernel/dt.c
@@ -126,7 +126,7 @@ int dt_map_dev(const void *fdt, int offs, vaddr_t *base, size_t *size)
 }
 
 /* Read a physical address (n=1 or 2 cells) */
-static paddr_t _fdt_read_paddr(const uint32_t *cell, int n)
+paddr_t _fdt_read_paddr(const uint32_t *cell, int n)
 {
 	paddr_t addr;
 

gradhocJcalvo/optee-graddeck

core/include/kernel/dt.h
@@ -163,6 +163,11 @@ int dt_enable_secure_status(void *fdt, int node);
  */
 
 /*
+ * Return the base address from a cell or (paddr_t)-1 in case of error
+ */
+paddr_t fdt_read_paddr(const uint32_t *cell, int n);
+
+/*
  * Return the base address for the "reg" property of the specified node or
  * (paddr_t)-1 in case of error
  */
@@ -316,6 +321,13 @@ static inline int dt_map_dev(const void *fdt __unused, int offs __unused,
 {
 	return -1;
 }
+
+static inline paddr_t fdt_read_paddr(const uint32_t *cell __unused,
+				     int n __unused)
+{
+	return -1;
+}
+
 
 static inline paddr_t fdt_reg_base_address(const void *fdt __unused,
 					   int offs __unused)

core/kernel/dt.c
@@ -146,7 +146,7 @@ int dt_map_dev(const void *fdt, int offs, vaddr_t *base, size_t *size,
 }
 
 /* Read a physical address (n=1 or 2 cells) */
-static paddr_t fdt_read_paddr(const uint32_t *cell, int n)
+paddr_t fdt_read_paddr(const uint32_t *cell, int n)
 {
 	paddr_t addr;
 