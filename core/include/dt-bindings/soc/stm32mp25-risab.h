4ms/optee-stm32mp25

dt-bindings: introduce RISAB device tree bindings
This change introduces RISABPROT macro which describes the RIF
configuration for a RISAB, and various defines.

Signed-off-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
Change-Id: I6a8ec34a59863657d9007fa5e5960f0b15274f54
Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/257006
Tested-by: Gatien CHEVALLIER <gatien.chevallier@st.com>
Reviewed-by: Gatien CHEVALLIER <gatien.chevallier@st.com>
Reviewed-by: CITOOLS <MDG-smet-aci-reviews@list.st.com>
Reviewed-by: Lionel DEBIEVE <lionel.debieve@foss.st.com>

core/include/dt-bindings/soc/stm32mp25-risab.h
@@ -1,0 +1,36 @@
+/* SPDX-License-Identifier: (GPL-2.0+ OR BSD-3-Clause) */
+/*
+ * Copyright (C) 2022, STMicroelectronics - All Rights Reserved
+ */
+
+#ifndef _DT_BINDINGS_STM32MP25_RISAB_H
+#define _DT_BINDINGS_STM32MP25_RISAB_H
+
+/* RISAB control modes */
+#define RIF_DDCID_DIS				0x0
+#define RIF_DDCID_EN				0x1
+
+#define RISAB_READ_LIST_SHIFT			8
+#define RISAB_WRITE_LIST_SHIFT			16
+#define RISAB_CFEN_SHIFT			24
+#define RISAB_DPRIV_SHIFT			25
+#define RISAB_SEC_SHIFT				26
+#define RISAB_DCCID_SHIFT			27
+#define RISAB_DCEN_SHIFT			31
+
+#define RISABPROT(delegate_en, delegate_cid, sec, default_priv, \
+		  enabled, cid_write_list, cid_read_list, cid_priv_list) \
+	(((delegate_en) << RISAB_DCEN_SHIFT) | \
+	 ((delegate_cid) << RISAB_DCCID_SHIFT) | \
+	 ((sec) << RISAB_SEC_SHIFT) | ((default_priv) << RISAB_DPRIV_SHIFT) | \
+	 ((enabled) << RISAB_CFEN_SHIFT) | \
+	 ((cid_write_list) << RISAB_WRITE_LIST_SHIFT) | \
+	 ((cid_read_list) << RISAB_READ_LIST_SHIFT) | (cid_priv_list))
+
+/* RISABPROT macro masks */
+#define RISAB_PLIST_MASK			GENMASK_32(7, 0)
+#define RISAB_RLIST_MASK			GENMASK_32(15, 8)
+#define RISAB_WLIST_MASK			GENMASK_32(23, 16)
+#define RISAB_DCCID_MASK			GENMASK_32(30, 27)
+
+#endif /* _DT_BINDINGS_STM32MP25_RISAB_H */

gradhocJcalvo/optee-graddeck

core/include/dt-bindings/soc/stm32mp25-risab.h
@@ -1,0 +1,36 @@
+/* SPDX-License-Identifier: (GPL-2.0+ OR BSD-3-Clause) */
+/*
+ * Copyright (C) 2022, STMicroelectronics - All Rights Reserved
+ */
+
+#ifndef _DT_BINDINGS_STM32MP25_RISAB_H
+#define _DT_BINDINGS_STM32MP25_RISAB_H
+
+/* RISAB control modes */
+#define RIF_DDCID_DIS				0x0
+#define RIF_DDCID_EN				0x1
+
+#define RISAB_READ_LIST_SHIFT			8
+#define RISAB_WRITE_LIST_SHIFT			16
+#define RISAB_CFEN_SHIFT			24
+#define RISAB_DPRIV_SHIFT			25
+#define RISAB_SEC_SHIFT				26
+#define RISAB_DCCID_SHIFT			27
+#define RISAB_DCEN_SHIFT			31
+
+#define RISABPROT(delegate_en, delegate_cid, sec, default_priv, \
+		  enabled, cid_read_list, cid_write_list, cid_priv_list) \
+	(((delegate_en) << RISAB_DCEN_SHIFT) | \
+	 ((delegate_cid) << RISAB_DCCID_SHIFT) | \
+	 ((sec) << RISAB_SEC_SHIFT) | ((default_priv) << RISAB_DPRIV_SHIFT) | \
+	 ((enabled) << RISAB_CFEN_SHIFT) | \
+	 ((cid_write_list) << RISAB_WRITE_LIST_SHIFT) | \
+	 ((cid_read_list) << RISAB_READ_LIST_SHIFT) | (cid_priv_list))
+
+/* RISABPROT macro masks */
+#define RISAB_PLIST_MASK			GENMASK_32(7, 0)
+#define RISAB_RLIST_MASK			GENMASK_32(15, 8)
+#define RISAB_WLIST_MASK			GENMASK_32(23, 16)
+#define RISAB_DCCID_MASK			GENMASK_32(30, 27)
+
+#endif /* _DT_BINDINGS_STM32MP25_RISAB_H */