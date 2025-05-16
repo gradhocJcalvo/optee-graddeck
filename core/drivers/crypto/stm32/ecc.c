gradhocJcalvo/optee-graddeck

commit bce68bf91574d1c7aedd2d309ee852fd9382ad33

crypto: stm32: PKA limit driver support to ECDSA and ECDH.
Fix regression of xtest 4006 testing TEE_ALG_SM2_PKE.
PKA only support ECDSA key generation ECDH key exchange.

Change-Id: I2a2fdbc0fdd20d89bbf9ea9c4604df818fcaf54e
Signed-off-by: Thomas Bourgoin <thomas.bourgoin@foss.st.com>
Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/362215

core/drivers/crypto/stm32/ecc.c
@@ -294,6 +294,11 @@ static TEE_Result stm32_alloc_keypair(struct ecc_keypair *s, uint32_t type,
 	if (!s)
 		return TEE_ERROR_BAD_PARAMETERS;
 
+	/* This driver only supports ECDH/ECDSA */
+	if (type != TEE_TYPE_ECDSA_KEYPAIR &&
+	    type != TEE_TYPE_ECDH_KEYPAIR)
+		return TEE_ERROR_NOT_IMPLEMENTED;
+
 	DMSG("Using PKA");
 	memset(s, 0, sizeof(*s));
 
@@ -322,6 +327,11 @@ static TEE_Result stm32_alloc_publickey(struct ecc_public_key *s, uint32_t type,
 {
 	if (!s)
 		return TEE_ERROR_BAD_PARAMETERS;
+
+	/* This driver only supports ECDH/ECDSA */
+	if (type != TEE_TYPE_ECDSA_KEYPAIR &&
+	    type != TEE_TYPE_ECDH_KEYPAIR)
+		return TEE_ERROR_NOT_IMPLEMENTED;
 
 	DMSG("Using PKA");
 	memset(s, 0, sizeof(*s));

commit 6183f7a3a355e669daf4fe193d11940cbd40d465

crypto: stm32: fix PKA ECDSA and ECDH limitation for public key
type given in parameter of .alloc_publickey ops is a _PUBLIC_KEY, not a
_KEYPAIR. Use then the right type to limit support to ECDSA and ECDH.

Fixes: bce68bf91574 ("crypto: stm32: PKA limit driver support to ECDSA and ECDH.")
Signed-off-by: Amelie Delaunay <amelie.delaunay@foss.st.com>
Change-Id: I6cd03ce6bc2fd69e8a74ffb2cb8bfe1b7479176d
Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/430840
Domain-Review: Yann GAUTIER <yann.gautier@foss.st.com>
ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
ACI: CIBUILD <MDG-smet-aci-builds@list.st.com>
Reviewed-by: Thomas BOURGOIN <thomas.bourgoin@foss.st.com>

core/drivers/crypto/stm32/ecc.c
@@ -329,8 +329,8 @@ static TEE_Result stm32_alloc_publickey(struct ecc_public_key *s, uint32_t type,
 		return TEE_ERROR_BAD_PARAMETERS;
 
 	/* This driver only supports ECDH/ECDSA */
-	if (type != TEE_TYPE_ECDSA_KEYPAIR &&
-	    type != TEE_TYPE_ECDH_KEYPAIR)
+	if (type != TEE_TYPE_ECDSA_PUBLIC_KEY &&
+	    type != TEE_TYPE_ECDH_PUBLIC_KEY)
 		return TEE_ERROR_NOT_IMPLEMENTED;
 
 	FMSG("Using PKA");