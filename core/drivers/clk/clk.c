gradhocJcalvo/optee-graddeck

commit 79591a69e1e58bb053200ce8cf78afd1f09132fb

clk: add CLK_SET_RATE_PARENT flags
Add CLK_SET_RATE_PARENT flags to propagate rate change up one level.

Change-Id: I3dcf67f5a37e342ff158482b779efb6361468aaf
Signed-off-by: Gabriel Fernandez <gabriel.fernandez@foss.st.com>

core/drivers/clk/clk.c
@@ -214,19 +214,34 @@ unsigned long clk_get_rate(struct clk *clk)
 
 static TEE_Result clk_set_rate_no_lock(struct clk *clk, unsigned long rate)
 {
-	TEE_Result res = TEE_ERROR_GENERIC;
+	TEE_Result res = TEE_SUCCESS;
 	unsigned long parent_rate = 0;
+	unsigned long new_rate = rate;
+	struct clk *parent = clk->parent;
 
-	if (clk->parent)
-		parent_rate = clk_get_rate(clk->parent);
+	if (parent)
+		parent_rate = parent->rate;
 
-	res = clk->ops->set_rate(clk, rate, parent_rate);
-	if (res)
-		return res;
+	if (!parent || !(clk->flags & CLK_SET_RATE_PARENT)) {
+		/* pass-through clock without adjustable parent */
+		new_rate  = rate;
+	} else {
+		/* pass-through clock with adjustable parent */
+		res = clk_set_rate_no_lock(parent, rate);
+		if (res)
+			return res;
+		new_rate = parent->rate;
+	}
+
+	if (clk->ops->set_rate) {
+		res = clk->ops->set_rate(clk, new_rate, parent_rate);
+		if (res)
+			return res;
+	}
 
 	clk_compute_rate_no_lock(clk);
 
-	return TEE_SUCCESS;
+	return res;
 }
 
 TEE_Result clk_set_rate(struct clk *clk, unsigned long rate)
@@ -234,8 +249,9 @@ TEE_Result clk_set_rate(struct clk *clk, unsigned long rate)
 	uint32_t exceptions = 0;
 	TEE_Result res = TEE_ERROR_GENERIC;
 
-	if (!clk->ops->set_rate)
-		return TEE_ERROR_NOT_SUPPORTED;
+	/* bail early if nothing to do */
+	if (rate == clk_get_rate(clk))
+		return TEE_SUCCESS;
 
 	exceptions =  cpu_spin_lock_xsave(&clk_lock);
 

core/include/drivers/clk.h
@@ -15,6 +15,7 @@
 #define CLK_SET_RATE_GATE	BIT(0) /* must be gated across rate change */
 #define CLK_SET_PARENT_GATE	BIT(1) /* must be gated across re-parent */
 #define CLK_OPS_PARENT_ENABLE	BIT(2) /* parent need enable during re-parent */
+#define CLK_SET_RATE_PARENT	BIT(3) /* propagate rate change up one level */
 
 /**
  * struct clk - Clock structure

commit 0aa89449ef848d0cc28b667f4facaac3d8aa770e

clk: error when set_rate ops is not supported
The function clk_set_rate() returns the error TEE_ERROR_NOT_SUPPORTED
if this operant is not supported by the clock, when the flag
CLK_SET_RATE_PARENT is absent and when the ops set_rate is absent.

This patch allows the caller to detect an issue when set_rate ops is not
performed without need to request again clk_get_rate().

clk_compute_rate_no_lock() is still called to recompute frequency as parent
rate can change.

Fixes: 79591a69e1e5 ("clk: add CLK_SET_RATE_PARENT flags")
Signed-off-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
Change-Id: I62b83a7e41a0d3a68bda164eca70af5e7706bdde
Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/443430
Reviewed-by: Gabriel FERNANDEZ <gabriel.fernandez@foss.st.com>
Reviewed-by: Thomas BOURGOIN <thomas.bourgoin@foss.st.com>
ACI: CIBUILD <MDG-smet-aci-builds@list.st.com>
ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>

core/drivers/clk/clk.c
@@ -58,6 +58,9 @@ static bool __maybe_unused clk_check(struct clk *clk)
 	if (clk->num_parents > 1 && !clk->ops->get_parent)
 		return false;
 
+	if (clk->ops->determine_rate && !clk->ops->set_rate)
+		return false;
+
 	return true;
 }
 
@@ -360,6 +363,7 @@ static TEE_Result clk_set_rate_no_lock(struct clk *clk, unsigned long rate)
 	unsigned long parent_rate = 0;
 	unsigned long new_rate = rate;
 	struct clk *parent = clk->parent;
+	bool skip_set_rate = false;
 
 	if (parent)
 		parent_rate = parent->rate;
@@ -407,6 +411,7 @@ static TEE_Result clk_set_rate_no_lock(struct clk *clk, unsigned long rate)
 		if (res)
 			return res;
 		new_rate = parent->rate;
+		skip_set_rate = true;
 	}
 
 	if (clk->ops->set_rate) {
@@ -420,6 +425,8 @@ static TEE_Result clk_set_rate_no_lock(struct clk *clk, unsigned long rate)
 
 		if (res)
 			return res;
+	} else if (!skip_set_rate) {
+		res = TEE_ERROR_NOT_SUPPORTED;
 	}
 
 	clk_compute_rate_no_lock(clk);